4f505b3a541b63c61d846bc741edf7be610ac39b
aaronbrunet <awbrunet@gmail.com>
2021-01-20 14:26:22 -0500

    Added removal and timestamps

--- src/App.js
+++ src/App.js
@@ -6,29 +6,59 @@ import { AddWordCount } from './components/AddWordCount'
 function App() {
   const [count,setCount] = useState(0)  
   const [countList,addCountList] = useState([])
+  const [lastUpdate,setLastUpdate] = useState('Never')
 
   const update = newCount => {
     setCount(newCount)
+    if(count < 0){setCount(0)}
+    updateTime()
   }
 
   const addToList = newCount => {
     const arr = countList
-    arr.push(newCount)
-    addCountList(arr)
+    const now = Date().toLocaleString()
+    //const length = arr.length
+    const wordcount = {}
+    wordcount.count = newCount
+    wordcount.time = now
+    arr.push(wordcount)
+    addCountList(arr)    
+    updateTime()
   }
 
-  const list = countList.map((item,index)=>(
-    <li className="index">{item}</li>
+  const updateTime = () => {
+    let newUpdate = countList.length>0 ? countList.reduce((max,wordcount) => (wordcount.time > max ? wordcount.time : max),countList[0].time) : 'Never'
+    setLastUpdate(newUpdate)
+  }
+
+  const removeFromList = (value,idx) => {    
+    let result = parseInt(count) - parseInt(value)
+    update(result)
+    let arr = countList
+    arr = arr.filter((item,i) => i !== idx)
+    addCountList(arr)
+    if (arr.length === 0) {
+      setCount(0)
+    }
+    updateTime()
+  }
+
+  const list = countList.map((wordcount,index)=>(
+    <li key={index} className='list'>{wordcount.count} <button onClick={() => removeFromList(wordcount.count,index)}>X</button>
+    <br/>{wordcount.time}</li>
   ))
+
+  //const lastUpdate = countList.length>0 ? countList.reduce((max,wordcount) => (wordcount.now > max ? wordcount.now: max),countList[0].now) : null
 
   return (
     <div className="App">
       <header className="App-header">
         <HelloWorld />        
-        <AddWordCount count={count} _setCount={update} _addList={addToList}/>
+        <AddWordCount count={count} _setCount={update} _addList={addToList} list={countList}/>
         <h3 className="count_h3">Word Count: { count }</h3>
+        <h4>Last Updated: {lastUpdate}</h4>
         <h3 className="count-list_h3">Word Count History</h3>
-        <ul>{list}</ul>
+        {countList.length>0 ? <ul>{list}</ul> : 'Add a wordcount above'}
       </header>      
     </div>
   );
--- src/components/AddWordCount.js
+++ src/components/AddWordCount.js
@@ -1,28 +1,21 @@
 import React, { useState } from 'react'
 
 export const AddWordCount = props => {
-    const [count,setCount] = useState(props.count)
     const [newCount,setNewCount] = useState(0)
 
     const addCount = newCount => {        
-        let result = parseInt(count) + parseInt(newCount)
+        let result = parseInt(props.count) + parseInt(newCount)
         setNewCount(0)
-        props._setCount(result)
-        setCount(result)
+        props._setCount(result)        
         props._addList(newCount) 
     }
 
-    const handleInputChange = (input) => {
-        let value = input    
-        setNewCount(value)
+    const handleInputChange = input => {
+        setNewCount(parseInt(input))
     }
 
     const handleClick = () => {
-        if (newCount != 0) {
-            addCount(newCount)        
-        } else {
-            setNewCount(0)
-        }
+        newCount !== 0 && addCount(newCount)        
     }
 
     return (
