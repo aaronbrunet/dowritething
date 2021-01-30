70ea771ab1001a6257f2cec885af2a008790e926
aaronbrunet <awbrunet@gmail.com>
2021-01-30 13:30:49 -0500

    Added basic filtering & fetching

--- src/App.js
+++ src/App.js
@@ -14,7 +14,13 @@ import { Title } from './components/Title'
 import { AddWordCount } from './components/AddWordCount'
 
 firebase.initializeApp({
-
+  apiKey: "AIzaSyDzG4HdlHob3LmULQ2MsKHo4ZCo1UzJPBQ",
+  authDomain: "dowritething-4c0f3.firebaseapp.com",
+  projectId: "dowritething-4c0f3",
+  storageBucket: "dowritething-4c0f3.appspot.com",
+  messagingSenderId: "530471616269",
+  appId: "1:530471616269:web:b98c7b996259f924243a44",
+  measurementId: "G-19L5Y8YZGM"
 })
 
 const auth = firebase.auth()
@@ -96,18 +102,68 @@ function App() {
 
   //const lastUpdate = countList.length>0 ? countList.reduce((max,wordcount) => (wordcount.now > max ? wordcount.now: max),countList[0].now) : null
 
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
       <header className="App-header">
-        <Title />        
-        <AddWordCount count={count} _setCount={update} _addList={addToList} list={countList}/>
-        <h3 className="count_h3">Word Count: { count }</h3>
-        <h4>Last Updated: {lastUpdate}</h4>
-        <h3 className="count-list_h3">Word Count History</h3>
-        {countList.length>0 ? <ul>{list}</ul> : 'Add a wordcount above'}
-      </header>      
+        <Title />
+      </header> 
+        <div id="main">
+          {user ?
+          <>
+          <SignOut />
+          <AddWordCount count={count} _setCount={update} _addList={addToList} list={countList}/>
+          <h3 className="count_h3">Word Count: { count }</h3>
+          <h4>Last Updated: {lastUpdate}</h4>
+          <h3 className="count-list_h3">Word Count History</h3>
+          {countList.length>0 ? <ul>{list}</ul> : 'Add a wordcount above'}
+          <WordCountList />
+          </>
+          :
+          <SignIn />
+          }
+
+        </div> 
+       
     </div>
   );
+}
+
+function SignIn() {
+  const signInAuth = () => {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      auth.signInWithPopup(provider)
+  }  
+
+  return (    
+    <button onClick={signInAuth}>Sign in</button>
+  )
+}
+
+function SignOut() {  
+  return auth.currentUser && (<>
+    <p>{auth.currentUser.uid}</p><button onClick={() => auth.signOut()}>Sign out</button>
+  </>)
+}
+
+function WordCountList() {
+  const wcRef = firestore.collection('wordcount')
+  const query = wcRef.where('uuid','==','notme').limit(20)
+  //const query = wcRef.orderBy('timestamp').limit(20)
+  const [wordcounts] = useCollectionData(query, {idField: 'id'})
+
+  return (
+    <>
+    <p>List</p>
+    {wordcounts && wordcounts.map(wc => <WordCount key={wc.uid} wordcount={wc}/>)}
+    </>
+  )
+}
+
+function WordCount(props) {
+  const {count,timestamp,uuid,project} = props.wordcount
+  return <p>Count: {count}; UUID: {uuid}</p>
 }
 
 export default App;
