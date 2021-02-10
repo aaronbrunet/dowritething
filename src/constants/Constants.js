a875c2e70b7be1fe152c889e2ce740c6baa64f67
aaronbrunet <awbrunet@gmail.com>
2021-02-09 23:14:17 -0500

    added basic Goal model support

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,2 +1,4 @@
 export const projectModel = [{name:'name',type:'text'},{name:'wordcount',type:'number'},{name:'description',type:'text'},{name:'created',type:'date'}]
 export const wcModel = [{name:'name',type:'text'},{name:'count',type:'number'},{name:'created',type:'date'}]
+//TODO
+export const goalModel = [{}]
