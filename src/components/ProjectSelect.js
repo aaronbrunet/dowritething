67ca52fe3f72df7f8c891222f2bd4913e494ee34
aaronbrunet <awbrunet@gmail.com>
2021-02-25 22:31:52 -0500

    Fixed default closed Project Select state

--- src/components/ProjectSelect.js
+++ src/components/ProjectSelect.js
@@ -5,7 +5,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { Select } from './Select'
 
 export const ProjectSelect = (props) => {    
-    const [isOpen,toggleOpen] = useState(true)
+    const [isOpen,toggleOpen] = useState(false)
     const projectRef = firestore.collection(`users/${auth.currentUser.uid}/projects`)  
     const query = projectRef.orderBy('name')
     const [projects] = useCollectionData(query,{idField: 'id'})
@@ -27,18 +27,17 @@ export const ProjectSelect = (props) => {
   
     return (
     <div id='project-select-container' className='container flex w-full justify-items-center items-center h-12'>
-    <button onClick={()=>toggleOpen(()=>!isOpen)} className='flex flex-col align-middle justify-center mr-4' title='Projects' aria-label='Select Projects'>{isOpen ? '📖':'📕' }</button>
-    {isOpen && 
-      <>
-        <Select 
-          name="Projects" 
-          options={projects} 
-          placeholder="Select Project" 
-          _onChange={setProject} 
-          selection={selection}     
-          />
-        <button onClick={()=>props._setEdit('edit')} className="flex flex-col h-3/4 justify-center align-middle text-center content-center items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white transition duration-300 ease-in-out">Edit</button>
-        <button onClick={()=>props._setAdd('add')} className="flex flex-col h-3/4 justify-center align-middle items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white  transition duration-300 ease-in-out">Add Project+</button>    
-      </>}
+      <button onClick={()=>toggleOpen(()=>!isOpen)} className='flex flex-col align-middle justify-center mr-4' title='Projects' aria-label='Select Projects'>{isOpen ? '📖':'📕' }</button>
+      <span className={isOpen ? 'flex-col items-center w-full' : 'hidden' }>
+          <Select 
+            name="Projects" 
+            options={projects} 
+            placeholder="Select Project" 
+            _onChange={setProject} 
+            selection={selection}     
+            />
+          <button onClick={()=>props._setEdit('edit')} className="flex inline-flex h-3/4 justify-center align-middle text-center content-center items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white transition duration-300 ease-in-out">Edit</button>
+          <button onClick={()=>props._setAdd('add')} className="flex inline-flex h-3/4 justify-center align-middle items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white  transition duration-300 ease-in-out">Add Project+</button>    
+      </span>
     </div>)
   }
--- src/components/Select.js
+++ src/components/Select.js
@@ -23,7 +23,7 @@ export const Select = ( props ) => {
     
     return (
         <>
-        <select className="inline-flex mr-4 flex-row justify-between w-48 px-2 py-2 text-gray-700 bg-white border-2 border-white rounded-md shadow focus:border-spring-wood-600" name={name} defaultValue={selected} value={selected} onChange={(e)=>handleOnChange(e.target.value)}>            
+        <select className="flex-col mr-4 justify-between w-48 px-2 py-2 text-gray-700 bg-white border-2 border-white rounded-md shadow focus:border-spring-wood-600" name={name} defaultValue={selected} value={selected} onChange={(e)=>handleOnChange(e.target.value)}>            
             <option key='0' className="w-48 py-2 my-2 bg-white rounded-lg shadow-xl" value='default' disabled>{placeholder}</option>
             {options && options.map(option=>
                 <option key={option.id} value={option.id} className="-48 py-2 my-2 bg-white rounded-lg shadow-xl"
