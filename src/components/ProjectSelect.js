febd9b448b3f035f39f26cda9e4ed7caeee7fc0a
aaronbrunet <awbrunet@gmail.com>
2021-02-25 22:06:48 -0500

    Added basic Project Select toggle

--- src/components/ProjectSelect.js
+++ src/components/ProjectSelect.js
@@ -5,6 +5,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { Select } from './Select'
 
 export const ProjectSelect = (props) => {    
+    const [isOpen,toggleOpen] = useState(true)
     const projectRef = firestore.collection(`users/${auth.currentUser.uid}/projects`)  
     const query = projectRef.orderBy('name')
     const [projects] = useCollectionData(query,{idField: 'id'})
@@ -17,25 +18,26 @@ export const ProjectSelect = (props) => {
       }
     } else {
       selection = props.defaultSelection
-    }
-    //!props.defaultSelection && projects ?  : findDefault = props.defaultSelection
+    }    
 
-    const setProject = (input) => {
-      //setValue(()=>input)
+    const setProject = (input) => {     
       props._setProject(input)
     }
 
   
-    return (<>
-      {/* <h1>Projects</h1> */}
-      <Select 
-        name="Projects" 
-        options={projects} 
-        placeholder="Select Project" 
-        _onChange={setProject} 
-        selection={selection}     
-        />
-      <button onClick={()=>props._setEdit('edit')} className="inline-flex text-center content-center items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white transition duration-300 ease-in-out">Edit</button>
-      <button onClick={()=>props._setAdd('add')} className="inline-flex items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white  transition duration-300 ease-in-out">Add Project+</button>
-    </>)
+    return (<div id='project-select-container' className='h-full m-2 align-middle content-center'>
+    <button onClick={()=>toggleOpen(()=>!isOpen)} className='inline-flex' title='Projects' aria-label='Select Projects'>{isOpen ? '📖':'📕' }</button>
+    {isOpen && 
+      <div id='project-select' className='inline-flex'>
+        <Select 
+          name="Projects" 
+          options={projects} 
+          placeholder="Select Project" 
+          _onChange={setProject} 
+          selection={selection}     
+          />
+        <button onClick={()=>props._setEdit('edit')} className="inline-flex text-center content-center items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white transition duration-300 ease-in-out">Edit</button>
+        <button onClick={()=>props._setAdd('add')} className="inline-flex items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white  transition duration-300 ease-in-out">Add Project+</button>    
+      </div>}
+    </div>)
   }
