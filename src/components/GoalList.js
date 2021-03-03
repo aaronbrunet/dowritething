03566278a7511be37d2d096758cd5cb37c70e000
aaronbrunet <awbrunet@gmail.com>
2021-03-02 21:10:40 -0500

    Added Modal for Goal addition

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { firestore } from '../firebase/firebase.js'
 
+import { Modal } from './Modal'
+
 const goal = {
   count: 0,
   timestamp: null,
@@ -16,7 +18,12 @@ export const GoalList = (props) => {
 
   const { currentUser, currentProject, goals, goalRef } = props
   const [currentGoal,setCurrentGoal] = useState(goal) 
+  const [edit,setEdit] = useState(false)  
     
+  const handleInputChange = input => {
+    setCurrentGoal({...currentGoal, count: parseInt(input)})
+  }
+
   const addGoal = newGoal => {     
       // const wcRef = firestore.collection(`users/${currentUser.uid}/projects/${currentProject.id}/wordcount`)
       goalRef.add({
@@ -28,25 +35,38 @@ export const GoalList = (props) => {
           repeat: newGoal.repeat,
           active: false,
           completed: false
-
       }).then(function(){
           console.log('New goal added!')          
         }).catch(function(error) {
           console.error('Error adding new count: '+error)
-        })
-         
+        })         
   } 
 
-  return (
+  
+
+  return (<>
     <div className="container bg-white w-auto shadow-md p-6 m-6">
-    <div className="inline-flex font-medium text-xl m-2">Goals</div>
-    {goals?.length<=3 && 
-    (<button className="m-2 inline-flex items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white transition duration-300 ease-in-out">
-      Add Goal</button>)}
-    {(!goals || (goals && goals.length === 0)) && (<p>No goal currently set.</p>)}
-    {goals && goals.map(goal => <Goal key={goal.uid} goal={goal}/>)}      
-    </div>    
-  )
+      <div className="inline-flex font-medium text-xl m-2">Goals</div>
+      {goals?.length<=3 && 
+      (<button onClick={()=>setEdit(()=>!edit)} className="m-2 inline-flex items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white transition duration-300 ease-in-out">
+        Add Goal</button>)}
+      {(!goals || (goals?.length === 0)) && (<p>No goal currently set.</p>)}
+      {goals?.map(goal => <Goal key={goal.uid} goal={goal}/>)}      
+    </div>  
+    {edit &&  
+      <Modal title='Add a New Goal'>
+        <input type="number" name="count" value={currentGoal.count} onChange={(e) => handleInputChange(e.target.value)} />   
+        <div className='button-row flex flex-row mt-4'>
+                <button className="flex flex-col items-center shadow bg-spring-wood-800 text-white text-xs rounded px-4 py-2 mr-2 hover:text-spring-wood-800 hover:bg-white">
+                  Add Progress
+                </button>
+                <button onClick={()=> setEdit(()=> !edit)} className="flex flex-col items-center shadow bg-white text-spring-wood-800 text-xs rounded px-4 py-2 hover:bg-spring-wood-800 hover:text-white">
+                  Cancel
+                </button>
+        </div>
+      </Modal>
+    }
+  </>)
 
   function Goal(props) {
     const {count,timestamp,type,repeat,start,end} = props.goal
